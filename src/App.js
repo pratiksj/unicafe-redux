adbe055d914dd498bd626f8a6853dbd2c2003184
pratiksj <pratiksharai728@gmail.com>
2022-11-30 12:14:31 +0545

    feat: refactoring the code

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useDispatch } from "react-redux";
+import React from "react";
+
+const App = () => {
+  const dispatch = useDispatch();
+  const good = () => {
+    dispatch({
+      type: "GOOD",
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={good}>good</button>
+      <button>ok</button>
+      <button>bad</button>
+      <button>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok</div>
+      <div>bad</div>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
-import reducer from './reducer'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
+import reducer from "./reducer";
+import { Provider } from "react-redux";
+import App from "./App";
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
-const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
+// const App = () => {
+//   const good = () => {
+//     store.dispatch({
+//       type: 'GOOD'
+//     })
+//   }
 
-  return (
-    <div>
-      <button onClick={good}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <button onClick={good}>good</button>
+//       <button>ok</button>
+//       <button>bad</button>
+//       <button>reset stats</button>
+//       <div>good {store.getState().good}</div>
+//       <div>ok</div>
+//       <div>bad</div>
+//     </div>
+//   )
+// }
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// const renderApp = () => {
+//   root.render(<App />)
+// }
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-const renderApp = () => {
-  root.render(<App />)
-}
-
-renderApp()
-store.subscribe(renderApp)
+// renderApp()
+// store.subscribe(renderApp)
