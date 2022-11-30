e79f4bdf615fe5193e7a42471860b53878ba2c90
pratiksj <pratiksharai728@gmail.com>
2022-11-30 13:52:13 +0545

    feat:refactored the reducer  funtion to make immutable

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import React from "react";
 
 const App = () => {
   const dispatch = useDispatch();
+  const note = useSelector((state) => state);
   const good = () => {
     dispatch({
       type: "GOOD",
     });
   };
-
+  const ok = () => {
+    dispatch({
+      type: "OK",
+    });
+  };
+  const bad = () => {
+    dispatch({
+      type: "BAD",
+    });
+  };
+  const reset = () => {
+    dispatch({
+      type: "RESET",
+    });
+  };
   return (
     <div>
       <button onClick={good}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <button onClick={ok}>ok</button>
+      <button onClick={bad}>bad</button>
+      <button onClick={reset}>reset stats</button>
+      {/* <div>good {store.getState().good}</div> */}
+      <div>good{note.good}</div>
+      <div>ok{note.ok}</div>
+      <div>bad{note.bad}</div>
     </div>
   );
 };
